25ab02af2187ce26dee43fbf182ca822c56c24a2
Izzy Stannett <isobel.stannett@gmail.com>
2018-06-10 17:20:03 +0100

    Edit profile activity added

--- app/src/main/java/com/example/android/cyclebuddy/EditProfileActivity.java
+++ app/src/main/java/com/example/android/cyclebuddy/EditProfileActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.cyclebuddy;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    //TODO: enable writing to database
+    //TODO: add changes listener with "Discard" dialogue
+
+    @BindView(R.id.edit_profile_toolbar) Toolbar editProfileToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        ButterKnife.bind(this);
+        setSupportActionBar(editProfileToolbar);
+
+        ActionBar ab = getSupportActionBar();
+        if(ab != null){
+            ab.setDisplayHomeAsUpEnabled(true);}
     }
 }
--- app/src/main/java/com/example/android/cyclebuddy/ProfileActivity.java
+++ app/src/main/java/com/example/android/cyclebuddy/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.cyclebuddy;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.android.cyclebuddy.R;
+import com.firebase.ui.auth.AuthUI;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +19,7 @@ import butterknife.ButterKnife;
 public class ProfileActivity extends AppCompatActivity {
 
     @BindView(R.id.profile_toolbar) Toolbar profileToolbar;
+    //TODO: enable reading from database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,9 @@ public class ProfileActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(profileToolbar);
 
+        ActionBar ab = getSupportActionBar();
+        if(ab != null){
+            ab.setDisplayHomeAsUpEnabled(true);}
     }
 
     @Override
@@ -38,10 +44,11 @@ public class ProfileActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.edit_profile:
-                Toast.makeText(this, "Editing Profile",Toast.LENGTH_SHORT).show();
+                Intent startEditProfile = new Intent(this, EditProfileActivity.class);
+                startActivity(startEditProfile);
                 return true;
             case R.id.sign_out:
-                Toast.makeText(this, "Signing Out",Toast.LENGTH_SHORT).show();
+                AuthUI.getInstance().signOut(this);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
